29bc0b46d92de27a985d59df95d4c311e1e97425
Rocky <yangrocky2013@gmail.com>
2020-10-08 21:55:17 -0500

    delete unneeded images, add projects and buttons.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,6 +5,10 @@ import Navbar from '../Navbar'
 import Skill from '../Skill'
 import Footer from '../Footer'
 import profilePic from '../../assets/images/profile-pic2.jpg'
+import chessVideo from '../../assets/videos/chess.mp4'
+import pokechampionsVideo from '../../assets/videos/pokechampions.mp4'
+import carlVideo from '../../assets/videos/carl.mp4'
+import swipefairVideo from '../../assets/videos/swipefair.mp4'
 
 const About = () => {
     let history = useHistory()
@@ -75,9 +79,14 @@ const About = () => {
                 </div>
                 <div className="skills-description-container">
                     <div className="extra-description-container">
-                        
+                        <div className="information-container">
+                            <h2>Rocky Yang</h2>
+                            <h3>Software Developer</h3>
+                            <span>You could say my passion as a programmer sparked when I took a CS class in college in my Fall 2015 semester. Because it was only an intro class I only learned arrays, loops, and functions.  We created fun games, such as tic-tac-toe, High or Low, and many other simple games.  I also had a blast solving little puzzles and enjoyed the feeling of solving a coding challenge.  Software developing makes me feel like I’m an artist. My screen is a blank canvas and my keyboard is my paintbrush.  Artists utilized different techniques and strokes to create a masterful painting and I do the same with my code to create a fully functional application out of nothing. If you’re looking for a software developer with experience in Python, JavaScript, React, and Express to complete your team, then you’ve found the right person.</span>
+                        </div>
                     </div>
                     <div className="skills-outer-container">
+                        <h2>Skills</h2>
                         <div className="skills-inner-container">
                             <Skill skill="JavaScript" percentage={85}/>
                             <Skill skill="CSS" percentage={80}/>
@@ -91,6 +100,84 @@ const About = () => {
                         </div>
                     </div>
                 </div>
+                <div className="projects-div-container">
+                    <div className="middle-body-header projects-middle">
+                        <h1>Projects</h1>
+                        <div className="underline-project"></div>
+                    </div>
+                    <div className="projects-body">
+                        <div className="chess-tab project-tab-left project-tab">
+                            <div className="project-video">
+                                <video src={chessVideo} autoplay="true" loop="true"></video>
+                            </div>
+                            <div className="project-description-container">
+                                <div className="project-description">
+                                    <h1>Chess</h1>
+                                    <h4>Chess is a two-player strategy board game that is played on an 8 x 8 checker-patterned game-board with 64 total squares.</h4>
+                                    <h4>JavaScript, React, HTML, CSS,  Express, Websockets</h4>
+                                    <div className="live-github-button">
+                                        <div className="live-button button">Live</div>
+                                        <div className="github-button button">GitHub</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="break-div"></div>
+                        <div className="pokechampions-tab project-tab-right project-tab">
+                            <div className="project-description-container">
+                                <div className="project-description">
+                                    <h1>Pokechampions</h1>
+                                    <h4>An application that allows the user to take on the Kanto League in the world of Pokemon.  Select six of your favorite Pokemon, and challenge the pokemon League and see if you can become the new Champion!</h4>
+                                    <h4>JavaScript, React, HTML, CSS,  Python, Flask, SQLAlchemy, PSQL, Postgres</h4>
+                                    <div className="live-github-button">
+                                        <div className="live-button button">Live</div>
+                                        <div className="github-button button">GitHub</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="project-video">
+                                <video src={pokechampionsVideo} autoplay="true" loop="true"></video>
+                            </div>
+                        </div>
+                        <div className="break-div"></div>
+                        <div className="carl-tab project-tab-left project-tab">
+                            <div className="project-video">
+                                <video src={carlVideo} autoplay="true" loop="true"></video>
+                            </div>
+                            <div className="project-description-container">
+                                <div className="project-description">
+                                    <h1>C.A.R.L.</h1>
+                                    <h4>CARL is a work-flow application to help users organize their tasks.  Tasks can be assigned to specific projects where they can be organized into categories.</h4>
+                                    <h4>JavaScript, React, HTML, CSS, PostgreSQL, Sequelize, Express, PUG, TDD</h4>
+                                    <div className="live-github-button">
+                                        <div className="live-button button">Live</div>
+                                        <div className="github-button button">GitHub</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="break-div"></div>
+                        <div className="swipefair-tab project-tab-right project-tab">
+                            <div className="project-description-container">
+                                <div className="project-description">
+                                    <h1>Swipefair</h1>
+                                    <h4> An application that combines the concepts of Tinder and LinkedIn. By being able to swipe whether you’re a company looking to hire or a jobseeker and will connect a jobseeker with a company when both swiped each other</h4>
+                                    <h4>JavaScript, React, HTML, CSS,  Python, Flask, SQLAlchemy, PSQL, Postgres</h4>
+                                    <div className="live-github-button">
+                                        <div className="live-button button">Live</div>
+                                        <div className="github-button button">GitHub</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="project-video">
+                                <video src={swipefairVideo} autoplay="true" loop="true"></video>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="contact-body">
+                
             </div>
             <Footer />
         </div>

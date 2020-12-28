3c20c32ab0bf5d45819f4d0c48805bb2444538ef
Rocky <yangrocky2013@gmail.com>
2020-12-28 14:27:24 -0600

    add react-ga for analytics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AppContext } from './context/AppContext'
 import Splash from './components/pages/Splash'
 import About from './components/pages/About'
+import ReactGA from 'react-ga';
 
 const App = () => {
 
   const [currentPage, setCurrentPage] = useState('')
   const [openedPortfolio, setOpenedPortfolio] = useState(false)
+
+  function initializeReactGA() {
+    ReactGA.initialize('UA-123791717-1');
+    ReactGA.pageview('/homepage');
+  }
+
+  useEffect(() => {
+    initializeReactGA()
+  }, [])
 
   return (
     <BrowserRouter>
